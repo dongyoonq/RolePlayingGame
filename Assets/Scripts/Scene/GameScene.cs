c65e7a44e84faeb7089bdc8f532b1989822af634
동윤 김 <ut_community@naver.com>
2023-06-14 21:49:39 +0900

    feat : Add Loading UI

---
+++ Assets/Scripts/Scene/GameScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+    private void Awake()
+    {
+        Debug.Log("GameScene Init");
+    }
+
+    protected override IEnumerator LoadingRoutine()
+    {
+        // fake loading
+        yield return new WaitForSecondsRealtime(0.2f);
+        progress = 0.2f;
+        yield return new WaitForSecondsRealtime(0.2f);
+        progress = 0.4f;
+        yield return new WaitForSecondsRealtime(0.2f);
+        progress = 0.6f;
+        yield return new WaitForSecondsRealtime(0.2f);
+        progress = 0.8f;
+        yield return new WaitForSecondsRealtime(0.2f);
+        progress = 1.0f;
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("GameScene Release");
+    }
+}
